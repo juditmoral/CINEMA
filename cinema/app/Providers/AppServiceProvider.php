3fa90d5c0905ade55416ec5496937fb52408ce48
juditmoral <juditmoralcampos@gmail.com>
2024-12-10 09:11:01 +0100

    esAdmin

--- cinema/app/Providers/AppServiceProvider.php
+++ cinema/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('administrar', function ($user) {
+            // una funció que retorna cert o fals segons si s'ha d'obrir o no la porta
+            return $user->esAdmin;
+        });
     }
 }
