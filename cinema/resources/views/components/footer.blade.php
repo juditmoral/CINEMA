2aea8ea2ecde308098e4b1ccddc78e0b40fa49d2
juditmoral <juditmoralcampos@gmail.com>
2025-01-18 17:26:41 +0100

    canvis

--- cinema/resources/views/components/footer.blade.php
+++ cinema/resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="w-screen h-20 bg-black text-white text-center py-3 fixed bottom-0">
   <div class="">
-    Web ideat i desenvolupat per Judit Moral, estudiant del 
+    Web ideat i desenvolupat per Judit Moral, estudiant del
     <a href="http://multimedia.uvic.cat/" target="_blank">
       Grau en Multimèdia, Aplicacions i Videojocs
     </a>
